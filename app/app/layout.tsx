6c326e118943b10516fdfdc6e7cf038ee6695388
kaganlove <kagan.love@yahoo.com>
2026-01-16 16:03:50 -0600

    Revert "Polish background and add accessible app surface"
    This reverts commit f677b46a58127e408bb2fb8c71193fcda965b87a.

--- app/app/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="appShell">
-      <div className="appPanel">{children}</div>
-    </div>
-  );
-}
